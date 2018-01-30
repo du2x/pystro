6668565e8c652df54461655026574e14e3099a53
du2x <dudumonteiro@gmail.com>
2018-01-30 10:30:16 -0200

    Initial commit fixes

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(BASEDIR, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     
