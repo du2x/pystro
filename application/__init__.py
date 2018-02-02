1156ebe45258035146ac3c1573e2c9f6a10eb373
du2x <dudumonteiro@gmail.com>
2018-02-02 10:26:56 -0200

    Few fixes

--- application/__init__.py
+++ application/__init__.py
@@ -9,19 +9,19 @@ from application.auth import authenticate, identity
 from application.database import db
 from application.resources.user import UserAPI, UsersAPI
 
+
 def set_api_routes(api):
     api.add_resource(UsersAPI, '/users', endpoint='users')
     api.add_resource(UserAPI, '/user/<int:id>', endpoint='user')    
 
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    #app.config.from_envvar('SMARTLUNCH_SETTINGS', silent=True)
+    app.config.from_envvar('SMARTLUNCH_SETTINGS', silent=True)
     app.app_context().push()
     with app.app_context():
         db.init_app(app)    
-    #migrate = Migrate(app, db)
+    migrate = Migrate(app, db)
     api = Api(app)
     set_api_routes(api)
     JWT(app, authenticate, identity)
--- smartlunch.py
+++ smartlunch.py
@@ -1,6 +1,5 @@
 from application import create_app
 from flask_jwt import jwt_required
 
-
 app, db = create_app()
 app.run()
